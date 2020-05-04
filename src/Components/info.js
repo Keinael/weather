2998f26828cc7b6bf9d96aa06b96286dfb89d9d8
Roman Usenko <usenko.ua@gmail.com>
2020-05-05 00:42:44 +0300

    Info component created

---
+++ src/Components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Wheather app</h2>
+                <p>Find your wheather</p>
+            </div>
+        )
+    };
+}
+
+export default Info;
